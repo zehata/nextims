41f7d06a81dc7c506e2bcdb56e165fcdc60382e4
zehata <zehata@gmail.com>
2026-03-05 20:09:27 +0800

    Added database deployment

--- configure.sh
+++ configure.sh
@@ -2,4 +2,7 @@
 
 minikube start --profile nextims
 minikube -p nextims docker-env
+minikube addons enable dashboard -p nextims
+minikube addons enable storage-provisioner -p nextims
+minikube addons enable storage-provisioner-rancher -p nextims
 skaffold dev
