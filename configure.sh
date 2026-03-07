220ea19562ecfbb716bb5f32af39573ffe68ee3b
zehata <zehata@gmail.com>
2026-03-07 22:49:14 +0800

    Configure Postgres service

--- configure.sh
+++ configure.sh
@@ -3,6 +3,5 @@
 minikube start --profile nextims
 minikube -p nextims docker-env
 minikube addons enable dashboard -p nextims
-minikube addons enable storage-provisioner -p nextims
 minikube addons enable storage-provisioner-rancher -p nextims
 skaffold dev
