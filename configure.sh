fe2f99dd3a0969492000b0cc451d49284e7fa8b8
zehata <zehata@gmail.com>
2026-03-16 18:37:37 +0800

    Add comment on allocating more memory to minikube

--- configure.sh
+++ configure.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Allocate more memory to minikube if you would like to
+# minikube config set memory 4096
 minikube start --profile nextims
 minikube -p nextims docker-env
 minikube addons enable dashboard -p nextims
